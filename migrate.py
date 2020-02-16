009b2b6a9bee496fd387212bbd19281ca967fd22
Alexander Holyoake <aholyoake@blockchain.com>
2020-02-16 20:20:20 +0000

    Add auth env vars

--- migrate.py
+++ migrate.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-# Must have python 2.7 installed
+# Must have python installed with requestite libs
 # This script assumes you have gcloud sdk installed and
 # the sdk has already be authenticated to gcloud
 # It also assumes access to your private repository through the REG_URL
 
 import requests
+from requests.auth import HTTPBasicAuth
 import json
 import logging
 import ast
@@ -15,8 +16,8 @@ os.path.abspath('/')
 
 ##########################################
 #  Examples for GCLOUD_URL AND REG_URL
-# export GCLOUD_URL="gcr.io/<project-name>/"
-# export REG_URL="docker-registry.example.com:5000/"
+# export GCLOUD_URL="gcr.io/<project-name>"
+# export REG_URL="docker-registry.example.com:5000"
 # export GCLOUDPATH="/usr/bin/gcloud"
 # export DOCKERPATH="/usr/bin/docker"
 # Make sure you have these env vars set
@@ -24,20 +25,32 @@ os.environ.get('GCLOUD_URL')
 os.environ.get('REG_URL')
 os.environ.get('GCLOUDPATH')
 os.environ.get('DOCKERPATH')
+os.environ.get('DOCKERUSER')
+os.environ.get('DOCKERPASSWORD')
 
 
-class MigrateToGcloud():
+class MigrateToGcloud:
     # Init some urls and paths for migration then call _get_catalog
     def __init__(self):
         self.REG_URL = os.environ.get('REG_URL')
         self.GCLOUD_URL = os.environ.get('GCLOUD_URL')
         self.dockerpath = os.environ.get('DOCKERPATH')
         self.gcloudpath = os.environ.get('GCLOUDPATH')
+        self.dockeruser = os.environ.get('DOCKERUSER', None)
+        self.dockerpassword = os.environ.get("DOCKERPASSWORD", None)
         self._get_catalog()
+
+    def _request(self,uri):
+        if self.dockeruser is not None:
+            auth = HTTPBasicAuth(self.dockeruser, self.dockerpassword)
+            return requests.get(uri, auth=auth)
+        else:
+            return requests.get(uri)
+
 
     # Get a catalog of repos from your existing repository
     def _get_catalog(self):
-        r = requests.get('https://' + self.REG_URL + '/v2/_catalog', auth('user', 'pass'))
+        r = self._request('https://' + self.REG_URL + '/v2/_catalog')
         logging.debug("Test get Catalog: %r", r)
         io = json.dumps(r.text)
         n = json.loads(io)
@@ -67,7 +80,7 @@ class MigrateToGcloud():
     # Get version tags from existing repository so we can migrate all of them
     def _get_tags(self, line):
         command = 'https://' + self.REG_URL + '/v2/' + line + '/tags/list'
-        checktags = requests.get(command, auth('user', 'pass'))
+        checktags = self._request.get(command)
         io = json.dumps(checktags.text)
         n = json.loads(io)
         tagline = ast.literal_eval(n)
